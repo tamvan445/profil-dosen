dae041959936403056e46eb25f52fa3378165fe8
wildan9 <optimus102001@gmail.com>
2021-01-02 18:01:22 +0700

    modify admin footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,9 +69,9 @@
     <!-- Main Footer -->
     <footer class="main-footer">
         <div class="float-right d-none d-sm-block">
-            <b>Version</b> 3.0.5
+            <b>Version</b> 0.0.1
         </div>
-        <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+        <strong>Copyright &copy; 2021 <a href="https://wildan9.github.io">wildan9.github.io</a>.</strong> All rights
         reserved.
     </footer>
 </div>
