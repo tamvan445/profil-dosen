7271556857394611126570c74460226389cf9756
wildan9 <optimus102001@gmail.com>
2021-01-03 23:35:50 +0700

    add college index method, route, & view

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,4 +4,8 @@
         <i class="nav-icon fas fa-home"></i>
         <p>Home</p>
     </a>
+    <a href="{{ route('colleges.index') }}" class="nav-link active">
+        <i class="nav-icon fas fa-university"></i>
+        <p>Perguruan Tinggi</p>
+    </a>
 </li>
