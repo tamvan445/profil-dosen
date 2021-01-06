1a805770f10b0c2d8f9e767011011d7f60cf4d92
wildan9 <optimus102001@gmail.com>
2021-01-06 21:37:07 +0700

    remove unused codes

--- resources/views/admin/colleges/edit.blade.php
+++ resources/views/admin/colleges/edit.blade.php
@@ -14,35 +14,31 @@
     </ol>
 </div>
 <div class="col-md-6 offset-md-3">
-        <form method="POST" action="{{route('college.update')}}" enctype="multipart/form-data">
-        @csrf
-        <input type="hidden" name="id" value="{{$college->id}}" />
-        <div class="form-group">
-            <label for="name" class="col-form-label">Perguruan Tinggi:</label>
-            <input type="text" class="form-control" name="name" value="{{ $college->name }}">
-        </div>
-        <div class="form-group">
-            <label for="accreditation" class="col-form-label">Akreditasi:</label>
-            <select name="accreditation" class="custom-select" required>
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" required>
-            <label class="form-check-label">
-                Saya setuju dengan syarat dan ketentuan
-            </label>
-            </div>
-        </div>
-                <button type="submit" class="btn btn-primary">Perbarharui</button>
-                </form>
-            </div>
-            </div>
+    <form method="POST" action="{{route('college.update')}}" enctype="multipart/form-data">
+    @csrf
+    <input type="hidden" name="id" value="{{$college->id}}" />
+    <div class="form-group">
+        <label for="name" class="col-form-label">Perguruan Tinggi:</label>
+        <input type="text" class="form-control" name="name" value="{{ $college->name }}">
+    </div>
+    <div class="form-group">
+        <label for="accreditation" class="col-form-label">Akreditasi:</label>
+        <select name="accreditation" class="custom-select" required>
+            <option value="A">A</option>
+            <option value="B">B</option>
+            <option value="C">C</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" required>
+        <label class="form-check-label">
+            Saya setuju dengan syarat dan ketentuan
+        </label>
         </div>
     </div>
+        <button type="submit" class="btn btn-primary">Perbarharui</button>
+    </form>
 </div>
 </section>
 @endsection
