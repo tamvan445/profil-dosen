d7bb6d4d046508161d1d4a7795ff6f595108468e
wildan9 <optimus102001@gmail.com>
2021-01-15 09:07:03 +0700

    change a method

--- database/migrations/2021_01_02_161356_create_lecturers_table.php
+++ database/migrations/2021_01_02_161356_create_lecturers_table.php
@@ -14,7 +14,7 @@ class CreateLecturersTable extends Migration
     public function up()
     {
         Schema::create('lecturers', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
             $table->string('nidn');
             $table->string('photo');
