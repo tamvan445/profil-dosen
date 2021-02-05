d622eabd38f7bb16e78166669b3c9bebd81a979e
wildan9 <optimus102001@gmail.com>
2021-02-06 03:18:38 +0700

    modify colleges views

--- resources/views/admin/colleges/edit.blade.php
+++ resources/views/admin/colleges/edit.blade.php
@@ -1,42 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" style="padding-top:10px;">
-    <div class="row">
-    <div class="col-sm-6">
-<p style="font-size: 20px;">Ubah Data Perguruan Tinggi</p>
-</div>
-<div class="col-sm-6">
-    <ol class="breadcrumb float-sm-right">
-        <li class="breadcrumb-item"><a  href="{{ route('colleges.index') }}">Data Peguruan Tinggi</a></li>
-        <li class="breadcrumb-item active">Edit</li>
-    </ol>
-</div>
-<div class="col-md-6 offset-md-3">
-    <form method="POST" action="{{ route('colleges.update') }}" enctype="multipart/form-data">
-    @csrf
-    <input type="hidden" name="id" value="{{$college->id}}" />
-    <div class="form-group">
-        <label for="name" class="col-form-label">Perguruan Tinggi:</label>
-        <input type="text" class="form-control" name="name" value="{{ $college->name }}">
-    </div>
-    <div class="form-group">
-        <label for="accreditation" class="col-form-label">Akreditasi:</label>
-        <select name="accreditation" class="custom-select" required>
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" required>
-        <label class="form-check-label">
-            Saya setuju dengan syarat dan ketentuan
-        </label>
+<div class="w-full h-screen overflow-x-hidden border-t flex flex-col">
+<main class="w-full flex-grow p-6">
+    <div class="flex flex-wrap">
+        <div class="w-full lg:w-1/2 my-6 pr-0 lg:pr-2">
+            <p class="text-xl pb-6 flex items-center">
+                <i class="fas fa-list mr-3"></i> Form Edit Perguruan Tinggi
+            </p>
+            <div class="leading-loose">
+            <div>
+            <ol class="list-none p-0 inline-flex">
+                <li class="flex items-center">
+                <a href="{{ route('colleges.index') }}">Tabel Perguruan Tinggi</a>
+                <svg class="fill-current w-3 h-3 mx-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"/></svg>
+                </li>
+                <li>
+                <a class="text-gray-500" aria-current="page">Edit Perguruan Tinggi</a>
+                </li>
+            </ol>
+            </div>
+
+                <form class="p-10 bg-white rounded shadow-xl" method="POST" action="{{ route('colleges.update') }}" enctype="multipart/form-data">
+                @csrf
+                <input type="hidden" name="id" value="{{$college->id}}" />
+                    <div class="pb-2">
+                        <label class="block text-sm text-gray-600" for="name">Perguruan Tinggi *</label>
+                        <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" name="name" type="text" required="" placeholder="Your Name" aria-label="Name" value="{{ $college->name }}">
+                    </div>
+                    <div class="pb-2">
+                    <label class="block text-sm text-gray-600" for="accreditation">Akreditasi *</label>
+                    <svg class="w-2 h-2 absolute right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
+                    <select name="accreditation" class="border border-gray-300 text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
+                        <option value="">Pilih Akreditasi</option>
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                        <option value="C">C</option>
+                    </select>
+                    </div>
+                    <div class="mt-6">
+                    <button class="rounded text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300">
+                        Simpan
+                    </button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-        <button type="submit" class="btn btn-primary">Perbarharui</button>
-    </form>
-</div>
+</main>
 @endsection
--- resources/views/admin/colleges/index.blade.php
+++ resources/views/admin/colleges/index.blade.php
@@ -1,87 +1,204 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid" style="margin-top: 15px">
-    <div class="card card-primary card-outline">
-        <div class="card-header">
-        <h3 class="card-title">Data Perguruan Tinggi</h3>
-        </div><!-- /.card-body -->
-        <div class="card-body">
-        <div class="float-left">
-        <button type="button" class="btn btn-block btn-outline-primary" data-toggle="modal" data-target="#addCollege" >+ Tambahkan Data</button>
-        </div><!-- /.button -->
-        <div class="float-right">
-        <form class="form-inline" style="margin-top: 15px;" method="GET" action="{{route('colleges.index')}}">
-            <input class="form-control mr-sm-2" type="text" name="search" placeholder="Cari Perguruan Tinggi" value="{{ request()->query('search') }}">
-            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Cari</button>
-        </form>
-        </div><!-- /.search perguruan tinggi -->
-        <table class="table table-hover text-nowrap">
-        <thead>
-        <tr>
-            <th style="width: 5px">No</th>
-            <th>Perguruan Tinggi</th>
-            <th style="width: 15px">Akreditasi</th>
-            <th style="width: 7.8em; text-align: center;">Edit | Delete</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ($colleges as $key => $college)
-        <tr>
-            <td>{{ $colleges->firstItem() + $key }}</td>
-            <td>{{ $college->name }}</td>
-            <td style="text-align: center;">{{ $college->accreditation }}</td>
-            <td>
-                <a type="button" href="/colleges/edit/{{$college->id}}" class="btn btn-info"><i class="fas fa-edit"></i></a>
-                <a type="button" href="/colleges/del/{{$college->id}}" onclick="return confirm('Are you sure you want to delete this item?');" class="btn btn-danger"><i class="fas fa-trash"></i></a>
-            </td>
-        </tr>
-        @endforeach
-        </tbody>
-    </table>
-    {{ $colleges->links('pagination::custom') }}
-        </div><!-- /.tabel perguruan tinggi -->
-    </div><!-- /.card-body -->
-</div><!-- /.container-fluid -->
-
-<div class="modal fade" id="addCollege" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Form Data Perguruan Tinggi</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form method="POST" action="{{ route('colleges.index') }}" enctype="multipart/form-data">
-        @csrf
-        <div class="form-group">
-            <label for="name" class="col-form-label">Perguruan Tinggi:</label>
-            <input type="text" class="form-control" name="name">
-          </div>
-        <div class="form-group">
-            <label for="accreditation" class="col-form-label">Akreditasi:</label>
-            <select name="accreditation" class="custom-select" required>
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-            </select>
-          </div>
-        <div class="form-group">
-            <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" required>
-            <label class="form-check-label">
-                Saya setuju dengan syarat dan ketentuan
-            </label>
-            </div>
+<div class="align-middle rounded-tl-lg rounded-tr-lg inline-block w-full py-4 overflow-hidden bg-gray-200 shadow-lg px-12">
+  <p class="pb-3 text-2xl">Data Perguruan Tinggi</p>
+  <div class="mb-4 flex justify-between items-center">
+    <div class="flex-1 pr-4">
+      <div class="relative md:w-1/3">
+      <form method="GET" action="{{ route('colleges.index') }}">
+        <input type="search"
+          class="w-full pl-10 pr-4 py-2 rounded-lg shadow focus:outline-none focus:shadow-outline text-gray-600 font-medium"
+          type="text" name="search" placeholder="Cari Perguruan Tinggi" value="{{ request()->query('search') }}">
+      </form>
+        <div class="absolute top-0 left-0 inline-flex items-center p-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-400" viewBox="0 0 24 24"
+            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+            stroke-linejoin="round">
+            <rect x="0" y="0" width="24" height="24" stroke="none"></rect>
+            <circle cx="10" cy="10" r="7" />
+            <line x1="21" y1="21" x2="15" y2="15" />
+          </svg>
         </div>
-          <div class="modal-footer">
-        <button type="submit" class="btn btn-primary">Tambahkan</button>
       </div>
-        </form>
+    </div>
+    <div>
+      <div class="shadow rounded-lg flex">
+        <div class="relative">
+        <button onclick="openModal()" class="rounded-lg inline-flex items-center bg-white hover:text-blue-500 focus:outline-none focus:shadow-outline text-gray-500 font-semibold py-2 px-2 md:px-4">
+              <i class="fas fa-plus mr-3"></i> Perguruan Tinggi
+          </button>
+        </div>
       </div>
     </div>
   </div>
-</div><!-- /.modal buat munculin form nambahin perguruan tinggi -->
+  <div class="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg">
+    <table class="min-w-full">
+      <thead>
+        <tr>
+          <th class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">No.</th>
+          <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Perguruan Tinggi</th>
+          <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider text-center">Akreditasi</th>
+          <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider text-center">Ubah || Hapus</th>
+        </tr>
+      </thead>
+      <tbody class="bg-white">
+      @foreach ($colleges as $key => $college)
+        <tr>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+            <div class="flex items-center">
+                <div>
+                    <div class="text-sm leading-5 text-gray-800">#{{ $colleges->firstItem() + $key }}</div>
+                </div>
+            </div>
+          </td>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+              <div class="text-sm leading-5 text-blue-900">{{ $college->name }}</div>
+          </td>
+          <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5 text-center">{{ $college->accreditation }}</td>
+          <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5 text-center">
+          <a type="button" href="/colleges/edit/{{$college->id}}"  class="px-2 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Ubah</i></a>    
+          <a type="button" href="/colleges/del/{{$college->id}}" onclick="return confirm('Are you sure you want to delete this item?');" 
+            class="px-2 py-2 border-red-500 border text-red-500 rounded transition duration-300 
+                    hover:bg-red-700 hover:text-white focus:outline-none">Hapus</a>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+      {{ $colleges->links('pagination::custom') }}
+    </div>
+  </div>
+</div>
+
+<!--Modal-->
+<div class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
+  style="background: rgba(0,0,0,.7);">
+  <div
+    class="border border-teal-500 shadow-lg modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+    <div class="modal-content py-4 text-left px-6">
+      <!--Title-->
+      <div class="flex justify-between items-center pb-3">
+        <p class="text-2xl font-bold">Form Perguruan Tinggi</p>
+        <div class="modal-close cursor-pointer z-50">
+          <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+            viewBox="0 0 18 18">
+            <path
+              d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+            </path>
+          </svg>
+        </div>
+      </div>
+      <!--Body-->
+      <div class="bg-white shadow rounded-lg p-6">
+    <div class="pb-4">
+    <form method="POST" action="{{ route('colleges.index') }}" enctype="multipart/form-data">
+        @csrf
+      <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+        <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+          <p>
+            <label for="name" class="bg-white text-gray-600 px-1">Perguruan Tinggi *</label>
+          </p>
+        </div>
+        <p>
+          <input name="name" autocomplete="false" tabindex="0" type="text" class="py-1 px-1 text-gray-900 outline-none block h-full w-full">
+        </p>
+      </div>
+      </div>
+      <div>
+      <label class="block text-sm text-gray-600" for="name">Akreditasi *</label>
+        <svg class="w-2 h-2 absolute right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
+        <select class="border border-gray-300 text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
+            <option value="">Pilih Akreditasi</option>
+            <option value="A">A</option>
+            <option value="B">B</option>
+            <option value="C">C</option>
+        </select>
+        </div>
+    </div>
+    <div class="border-t mt-6 pt-3">
+      <button class="rounded text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300">
+        Simpan
+      </button>
+    </div>
+  </form>
+  </div>
+  </div>
+</div>
+
+<style>
+  .animated {
+    -webkit-animation-duration: 1s;
+    animation-duration: 1s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+  }
+
+  .animated.faster {
+    -webkit-animation-duration: 500ms;
+    animation-duration: 500ms;
+  }
+
+  .fadeIn {
+    -webkit-animation-name: fadeIn;
+    animation-name: fadeIn;
+  }
+
+  .fadeOut {
+    -webkit-animation-name: fadeOut;
+    animation-name: fadeOut;
+  }
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeOut {
+    from {
+      opacity: 1;
+    }
+
+    to {
+      opacity: 0;
+    }
+  }
+</style>
+
+<script>
+  const modal = document.querySelector('.main-modal');
+  const closeButton = document.querySelectorAll('.modal-close');
+
+  const modalClose = () => {
+    modal.classList.remove('fadeIn');
+    modal.classList.add('fadeOut');
+    setTimeout(() => {
+      modal.style.display = 'none';
+    }, 500);
+  }
+
+  const openModal = () => {
+    modal.classList.remove('fadeOut');
+    modal.classList.add('fadeIn');
+    modal.style.display = 'flex';
+  }
+
+  for (let i = 0; i < closeButton.length; i++) {
+
+    const elements = closeButton[i];
+
+    elements.onclick = (e) => modalClose();
+
+    modal.style.display = 'none';
+
+    window.onclick = function (event) {
+      if (event.target == modal) modalClose();
+    }
+  }
+</script>
 @endsection
