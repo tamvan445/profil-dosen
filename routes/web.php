ac4cfc741bfe6dcd15d03ce612fe034545a4cc2a
wildan9 <optimus102001@gmail.com>
2021-02-10 14:18:58 +0700

    styling

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\HomeController;
-use App\Http\Controllers\Admin\CollegeController;
-use App\Http\Controllers\Admin\LecturerController;
-use App\Http\Controllers\Admin\CourseController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 /**
  * Author: Wildan R.
  * 2021
  * Made with Laravel framework 8.20.1
  */
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\CollegeController;
+use App\Http\Controllers\Admin\LecturerController;
+use App\Http\Controllers\Admin\CourseController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,25 +18,23 @@ Route::get('/', function () {
 
 Auth::routes();
 
-/*****Admin*********************************************************************************** */
+/* Admin Routes*/
 
-// Auth Midleware (Admin)
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function () { /* Auth Midleware (Admin) */
 
-// Home
+/* Home */
+
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 /**
  * College Routes
  */
+
 Route::prefix('colleges')->group(function () {
-// index and store
     Route::get('/', [CollegeController::class, 'index'])->name('colleges.index');
     Route::post('/', [CollegeController::class, 'store']);
-// update
     Route::get('/edit/{id}', [CollegeController::class, 'edit']);
     Route::post('/edit', [CollegeController::class, 'update'])->name('colleges.update');
-// delete
     Route::get('/destroy/{id}', [CollegeController::class, 'destroy']);
 });
 
@@ -56,17 +43,12 @@ Route::prefix('colleges')->group(function () {
  */
 
 Route::prefix('lecturers')->group(function () {
-// index
     Route::get('/', [LecturerController::class, 'index'])->name('lecturers.index');
-// store
     Route::get('/add', [LecturerController::class, 'create'])->name('lecturers.create');
     Route::post('/add', [LecturerController::class, 'store']);
-// update
     Route::get('/edit/{id}', [LecturerController::class, 'edit']);
     Route::post('/edit', [LecturerController::class, 'update'])->name('lecturers.update');
-// delete
     Route::get('/destroy/{id}', [LecturerController::class, 'destroy']);
-// show
     Route::get('/show/{id}', [LecturerController::class, 'show']);
 });
 
@@ -75,15 +57,12 @@ Route::prefix('lecturers')->group(function () {
  */
 
 Route::prefix('courses')->group(function () {
-// index and store
     Route::get('/', [CourseController::class, 'index'])->name('courses.index');
     Route::post('/', [CourseController::class, 'store']);
-// update
     Route::get('/edit/{id}', [CourseController::class, 'edit']);
     Route::post('/edit', [CourseController::class, 'update'])->name('courses.update');
-// delete
     Route::get('/destroy/{id}', [CourseController::class, 'destroy']);
 });
-/*****Admin*********************************************************************************** */
+/* Admin */
 
-}); /***Auth Midleware****** */
+/* Auth Midleware (Admin) */ }); 
