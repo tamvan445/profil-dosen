8d3646f9891d8a64969efb0eef0fa1fa89a71b75
wildan9 <optimus102001@gmail.com>
2021-02-12 23:40:13 +0700

    remove unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 // tailwind.config.js
 module.exports = {
-    purge: [],
     purge: [
         './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
 ],
     darkMode: false, // or 'media' or 'class'
     theme: {
