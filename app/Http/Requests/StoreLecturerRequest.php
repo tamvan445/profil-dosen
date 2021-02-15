254c83d725a45377f0e2e887381c48af8840f39e
wildan9 <optimus102001@gmail.com>
2021-02-16 03:29:34 +0700

    add lecturer form requests

---
+++ app/Http/Requests/StoreLecturerRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreLecturerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'nidn' => 'required|unique:lecturers',
+            'photo' => 'dimensions:min_width=100,min_height=200',
+            'college_id' => 'required',
+            'studyProgram' => 'required',
+            'gender' => 'required', 
+            'lastEducation' =>'required'
+        ];
+    }
+}
